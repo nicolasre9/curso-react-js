588767c7b5b365d343abf14ccef00f89c862da6e
nicolasre9 <45719908+nicolasre9@users.noreply.github.com>
2022-04-09 19:39:21 -0300

    Fin clase 3

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,6 +5,7 @@ import { Clicker } from '../../Ejemplos/Clicker';
 import { Producto } from '../../Ejemplos/Producto';
 import { pedirDatos } from '../../helpers/pedirDatos';
 import { stock } from '../../data/stock';
+import { Item } from '../Item/item';
 
 
 export const ItemListContainer = ( {greeting} ) => {
@@ -16,8 +17,19 @@ export const ItemListContainer = ( {greeting} ) => {
     // }
 
     const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(false)
+
+
+    // console.table(stock.map((el) => {
+    //     return {
+    //         nombre: el.nombre,
+    //         precio: el.precio
+    //     }
+    // }))
 
     useEffect(() =>{
+        setLoading(true)
+
         pedirDatos()
             .then((res) => {
                 setProductos(res);
@@ -25,20 +37,28 @@ export const ItemListContainer = ( {greeting} ) => {
             .catch((err) => {
                 console.log(err);
             })
+            .finally(()=> setLoading(false))
     },[])
     
 
 
     return (
         <section className="item-list-container">
-            <h2>{greeting}</h2>
-            <hr/>
-            {/* <Boton click={mostrar}>clicker</Boton> */}
-            {JSON.stringify(productos)}
-            <hr/>
+            {/* <h2>{greeting}</h2>
+            <hr/> */}
 
-            {/* { clicker === true ? <Clicker/> : null } */}
+            {
+                loading 
+                    ?   <h3>Loading...</h3>
+                    :   <div className="row">
+                            {/* { productos.map( (el, i) => <Item key={i} nombre={el.nombre} precio={el.precio} img={el.img} desc={el.desc} /> ) } */}
+                            { productos.map( (el, i) => <Item key={i} {...el}/> ) }
+                        </div>
+            }
             
+            {/* JSON.stringify(productos) */}
+            {/* <Boton click={mostrar}>clicker</Boton> */}
+            {/* { clicker === true ? <Clicker/> : null } */}
             {/* <Producto title={obj.titulo} content={obj.contenido}/> */}
         </section>
     )
